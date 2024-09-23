75cfb9118c4867ea5c7f588fd1096f3a5dc678fb
Jake <jakeplatt176290@gmail.com>
2024-09-23 14:53:14 -0400

    2

--- tools/scripts/post-install.sh
+++ tools/scripts/post-install.sh
@@ -11,6 +11,6 @@ curl https://get.volta.sh | bash
 #install node using volta from the absolute path
 $HOME/.volta/bin/volta install node
 
-echo "All Done! You should run this command to finish: source ~/.bashrc"
+echo "All Done! You should run this command to finish: "source ~/.bashrc"
 
 
