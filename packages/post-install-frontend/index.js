e2e5a8ae5aafbbd8c90e77c7c955599386362035
Jake <jakeplatt176290@gmail.com>
2024-09-25 08:05:13 -0400

    making port & URL config

--- packages/post-install-frontend/index.js
+++ packages/post-install-frontend/index.js
@@ -1,10 +1,13 @@
 const express = require("express"); //imports express module/library, gives us a funct
 const app = express(); //create express app, call the express funct (listens to specific port & create route handlers)
 
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || "https://raw.githubusercontent.com/JakeRyanPlatt/avg-C-dev/refs/heads/main/tools/scripts/post-install.sh | bash";
+
 app.get("/",function(req,res) {
-    return res.send("wget -qO- https://raw.githubusercontent.com/JakeRyanPlatt/avg-C-dev/refs/heads/main/tools/scripts/post-install.sh | bash")
+    return res.send(`wget -qO- ${URL} | bash`);
 });
 
-app.listen(3000, function() {
-    console.log("Hello World!");
+app.listen(PORT, function() {
+    console.log(`Server is listening on ${PORT}`);
 }); //Listen to a port (3000)
